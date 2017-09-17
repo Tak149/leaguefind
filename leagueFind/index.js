b31770a2a510527dbdd020e274633e9e7de24180
Jack <Tak149@users.noreply.github.com>
2017-09-17 16:51:29 -0400

    Add files via upload

--- leagueFind/index.js
+++ leagueFind/index.js
@@ -1,14 +1,30 @@
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-}
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+function togNav() {
+  var nav = document.getElementById("mySidenav");
+  if (nav.style.width == '100%') {
+    nav.style.width = '0';
+    nav.style.opacity = 0;
+  } else {
+    nav.style.width = "100%";
+    nav.style.opacity = .8;
+  }
 }
 
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+
+
+/* Set the width of the side navigation to 250px 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+*/
