94f39dde1d05735c0377d4faa45bc9f92a1cb428
Jack <Tak149@users.noreply.github.com>
2017-09-17 22:01:32 -0400

    Add files via upload

--- leagueFind/index.js
+++ leagueFind/index.js
@@ -15,16 +15,3 @@ function myFunction(x) {
     x.classList.toggle("change");
 }
 
-
-
-/* Set the width of the side navigation to 250px 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-}
-
-/* Set the width of the side navigation to 0 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-*/
