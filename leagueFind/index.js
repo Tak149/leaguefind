4b19e1c04e12e401bc34c498af63ea81474c8c0f
Jack <Tak149@users.noreply.github.com>
2017-09-17 23:34:47 -0400

    Add files via upload

--- leagueFind/index.js
+++ leagueFind/index.js
@@ -3,7 +3,7 @@
 function togNav() {
   var nav = document.getElementById("mySidenav");
   if (nav.style.width == '100%') {
-    nav.style.width = '0';
+    nav.style.width = '0%';
     nav.style.opacity = 0;
   } else {
     nav.style.width = "100%";
